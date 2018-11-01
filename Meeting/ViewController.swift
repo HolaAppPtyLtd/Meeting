8bd0c4dbeb322d1da0a8a208a4822925d33f5da3
YING CHEN <ying.chen@naamtech.com>
2018-11-01 15:05:43 +1100

    Barcode detect demo
    Add barcode detect demo

--- Meeting/ViewController.swift
+++ Meeting/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
+// [START import_vision]
+import Firebase
+// [END import_vision]
+
 class ViewController: UIViewController {
 
+    /// Firebase vision instance.
+    // [START init_vision]
+    lazy var vision = Vision.vision()
+    // [END init_vision]
+    
+    /// A string holding current results from detection.
+    var resultsText = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        detectBarcodes(image: #imageLiteral(resourceName: "barcode_128.png"))
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +35,47 @@ class ViewController: UIViewController {
     }
 
 
+    /// Detects barcodes on the specified image and draws a frame around the detected barcodes using
+    /// On-Device barcode API.
+    ///
+    /// - Parameter image: The image.
+    func detectBarcodes(image: UIImage?) {
+        guard let image = image else { return }
+        
+        // Define the options for a barcode detector.
+        // [START config_barcode]
+        let format = VisionBarcodeFormat.all
+        let barcodeOptions = VisionBarcodeDetectorOptions(formats: format)
+        // [END config_barcode]
+        // Create a barcode detector.
+        // [START init_barcode]
+        let barcodeDetector = vision.barcodeDetector(options: barcodeOptions)
+        // [END init_barcode]
+        // Define the metadata for the image.
+        let imageMetadata = VisionImageMetadata()
+        
+        // Initialize a VisionImage object with the given UIImage.
+        let visionImage = VisionImage(image: image)
+        visionImage.metadata = imageMetadata
+        
+        // [START detect_barcodes]
+        barcodeDetector.detect(in: visionImage) { features, error in
+            guard error == nil, let features = features, !features.isEmpty else {
+                // [START_EXCLUDE]
+                print(error ?? "Error")
+                return
+            }
+            
+            // [START_EXCLUDE]
+            self.resultsText = features.map { feature in
+                    return "\(feature.displayValue ?? "")"
+                }.joined(separator: "\n")
+            // [END_EXCLUDE]
+
+            print(self.resultsText )
+            
+        }
+        // [END detect_barcodes]
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Meeting' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Meeting
+pod 'Firebase/Core'
+pod 'Firebase/MLVision'
+pod 'Firebase/MLVisionBarcodeModel'
+
+  target 'MeetingTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MeetingUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
