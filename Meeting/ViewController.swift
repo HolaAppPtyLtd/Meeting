e425798610432df9b41b2a52740c850d818bc533
YING CHEN <ying.chen@naamtech.com>
2018-11-01 11:04:28 +1100

    Initial Commit

---
+++ Meeting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meeting
+//
+//  Created by YING CHEN on 1/11/18.
+//  Copyright © 2018 YING CHEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
